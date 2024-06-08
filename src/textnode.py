44d3bb2d5617a044042260c592e4974c1b41eacb
alecthedev <alecthedeveloper@gmail.com>
2024-06-07 21:04:48 -0400

    implement TextNode to HTMLNode conversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
 
     def __init__(self, text, text_type, url=None):
@@ -22,3 +24,12 @@ class TextNode:
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
+    def text_node_to_html_node(text_node):
+        match text_node.text_type:
+            case "text":   return LeafNode(None, text_node.text)
+            case "bold":   return LeafNode("b", text_node.text)
+            case "italic": return LeafNode("i", text_node.text)
+            case "code":   return LeafNode("code", text_node.text)
+            case "link":   return LeafNode("a", text_node.text, {"href": text_node.url})
+            case "image":  return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        raise ValueError(f"Invalid TextNode type: {text_node.text_type}")
