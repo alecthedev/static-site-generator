843567b688a737a3c63acd5b504e6dce5820aceb
alecthedev <alecthedeveloper@gmail.com>
2024-06-29 13:58:45 -0400

    implement markdown to block_type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -7,8 +7,39 @@ block_type_ordered_list = "ordered_list"
 
 
 def markdown_to_blocks(markdown):
-    return markdown.split("\n\n")
+    return markdown.strip().split("\n\n")
 
 
 def block_to_block_type(block):
-    pass
+    lines = block.split("\n")
+
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+
+    if any(line.startswith(">") for line in lines):
+        return block_type_quote
+
+    if any(line.startswith("* ") for line in lines) or any(
+        line.startswith("- ") for line in lines
+    ):
+        return block_type_unordered_list
+
+    if block.startswith("1. "):
+        line_num = 1
+        for line in lines:
+            if not line.startswith(f"{line_num}. "):
+                break
+            line_num += 1
+        return block_type_ordered_list
+
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -4,6 +4,7 @@ from markdown_blocks import *
 
 
 class TestMarkdownBlocks(unittest.TestCase):
+
     def test_markdown_to_blocks(self):
         markdown = """This is a **bolded** paragraph
 
@@ -20,3 +21,31 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_block_to_block_type_heading(self):
+        heading = "### This is a heading"
+        self.assertEqual(block_to_block_type(heading), block_type_heading)
+
+    def test_block_to_block_type_code(self):
+        code = "```\ndef code_block():\n\tprint('this is code')```"
+        self.assertEqual(block_to_block_type(code), block_type_code)
+
+    def test_block_to_block_type_quote(self):
+        quote = "> Make it a great day, or not.\n> The choice is yours."
+        self.assertEqual(block_to_block_type(quote), block_type_quote)
+
+    def test_block_to_block_type_unordered_list_dash(self):
+        ul = "- This is an unordered list\n- using dashes"
+        self.assertEqual(block_to_block_type(ul), block_type_unordered_list)
+
+    def test_block_to_block_type_unordered_list_ast(self):
+        ul = "* Also is an unordered list\n* using asterix"
+        self.assertEqual(block_to_block_type(ul), block_type_unordered_list)
+
+    def test_block_to_block_type_ordered_list(self):
+        ol = "1. first item\n2. second item\n3. third item"
+        self.assertEqual(block_to_block_type(ol), block_type_ordered_list)
+
+    def test_block_to_block_type_paragraph(self):
+        p = "Just a normal paragraph\nNothing to be concerned about."
+        self.assertEqual(block_to_block_type(p), block_type_paragraph)
